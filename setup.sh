22f34a4e14f4104ae9d6fd1441c280e93d35e9b6
Davide Ceretti <dav.ceretti@gmail.com>
2015-03-25 16:19:36 +0000

    Add sublime stuff

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# Vim
+
 rm -rf ~/.vimrc
 ln -s $(pwd)/vim/.vimrc ~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -5,3 +7,7 @@ vim +PluginInstall +qall
 sudo apt-get install -y build-essential cmake python-dev
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
+
+# Sublime
+ln -s $(pwd)/sublime/ipdb.sublime-snippet ~/.config/sublime-text-3/Packages/User/ipdb.sublime-snippet
+ln -s $(pwd)/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
